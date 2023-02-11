1dcdd98e4387d1c1d92a566626a98b038e5c87f7
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-02-11 15:59:32 +0530

    added css and routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
-import {Switch, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
+import {Home, CreatePost, PostDetail, Navbar} from './';
 
 function App() {
   return (
     <div className='container'>
-
-      <Switch>
+      
+      <Routes>
         <Route exact path ='/' component = {Home} />
-        <Route exact path ='/post/:postID' component = {PostDetail} />
-        <Route exact path ='/create-post' component = {CreatePost} />
-      </Switch>
+        <Route path ='/post/:postID' component = {PostDetail} />
+        <Route path ='/create-post' component = {CreatePost} />
+      </Routes>
 
-    </div>>
-  );
+    </div>
+  )
 }
 
 export default App;
