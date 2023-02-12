0a22197adf18255666f897aac97d7dae770da42c
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-02-12 14:35:44 +0530

    added post details component

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import {db} from '../firebase';
+import { getDoc, doc } from "firebase/firestore";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function PostDetails() {
+  const [post, setPost] = useState({});
+  const { postId } = useParams();
+
+  useEffect(() => {
+    const colRef = doc(db, 'posts', postId);
+    console.log(colRef);
+    getDoc(colRef)
+      .then((doc) => {
+        setPost(doc.data())
+      })
+  }, []);
   return (
-    <div>PostDetails</div>
+    <div className='post-details'>
+
+      <h1>{post.title}</h1>
+      <h3>{post.subTitle}</h3>
+      
+      <p>{post.content}</p>
+
+    </div>
   )
 }
 
